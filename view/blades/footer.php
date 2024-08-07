bd49d72d2a7ec54860be73c8c316e91ff83f4d04
Gustavo Silva <97358905+gsfsilva@users.noreply.github.com>
2024-08-07 10:18:10 -0300

    Add files via upload

---
+++ view/blades/footer.php
@@ -0,0 +1,57 @@
+<footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <h1>W<span>omen'</span>S F<span>oru</span>M</h1>
+                <p>Acredite que você pode, assim você já está no meio do caminho.</p>
+
+                <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="github">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </div>
+            </div>
+            
+            <ul class="footer-list">
+                <li>
+                    <h3>Disque Denúncia</h3>
+                </li>
+                <li>
+                    <a href="https://api.whatsapp.com/send/?phone=556196100180&text=oi&type=phone_number&app_absent=0" class="footer-link">whatsapp para Ajuda</a>
+                </li>
+            </ul>
+
+            <ul class="footer-list">
+                <li>
+                    <h3>Colaboradores</h3>
+                </li>
+                <li>
+                    <a href="https://github.com/anacardozo" class="footer-link">Ana Flávia: Web developer</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Geovanna: Mobile developer</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Ana Kelly: UX/UI Designer</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Gustavo: UX/UI Designer</a>
+                </li>
+            </ul>
+
+            <div id="footer_copyright">
+                &#169; 2024 all rights reserved
+            </div>
+        </div>
+    </footer> <!--end-footer-->
+    
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ view/index.php
@@ -0,0 +1,68 @@
+<?php include("blades/header.php");?>
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>Women's Forum</title>
+</head>
+<body>
+    <main>
+        <section class="home">
+            <div class="home-text">
+                <h4 class="text-h4">Bem vindo ao Women's Forum</h4>
+                <h1 class="text-h1"><span>Todos</span> Contra o <span>Feminicídio</span></h1>
+                <p>"Se alguém disser que ninguém vai acreditar em você, faça o teste! Não se cale diante da violência!"</p>
+                <a href="https://www.gov.br/mdh/pt-br/assuntos/denuncie-violencia-contra-a-mulher/violencia-contra-a-mulher" class="home-btn">Denuncie!</a>
+            </div>
+            <div class="home-img">
+                <img src="../img/img-header.jpg" alt="mulher">
+            </div>
+        </section>
+    </main>
+
+    <section class="sobre">
+        <div class="interface">
+            <div class="flex">
+                <div class="img-sobre">
+                    <img src="../img/home 1.png" alt="">
+                </div><!--img-sobre-->
+
+                <div class="txt-sobre">
+                    <h2>Casas de <span>Abrigo para Mulheres.</span></h2>
+                    <p>Mais do que um local seguro, a Casa Abrigo é um lugar de recomeço. De lá, as mulheres podem buscar uma nova vida, com mais oportunidades e sem violência. A Casa Abrigo é um espaço de garantia de defesa e proteção de mulheres vítimas de violência doméstica, familiar e sexual, em risco de morte, e de seus dependentes. A unidade oferece atendimento psicológico, jurídico, pedagógico e de assistência social.</p>
+                    <p>O ingresso no espaço é feito por meio de encaminhamento da Delegacia Especializada no Atendimento à Mulher (DEAM), demais delegacias, pela Casa da Mulher Brasileira ou por ordem judicial. As mulheres podem ir acompanhadas de seus filhos ou dependentes. Do sexo masculino, até doze anos de idade incompletos, e do sexo feminino, sem limitação de idade. Durante o período na Casa, as mulheres e seus dependentes recebem cuidados na área da saúde, orientações para o retorno ao mercado de trabalho e direcionamento escolar, a depender das necessidades de cada caso.</p>
+                    <div class="btn-social">
+                        <a href="#"><button><i class="fa-brands fa-instagram"></i></button></a>
+                        <a href="#"><button><i class="fa-brands fa-facebook-f"></i></button></a>
+                        <a href="#"><button><i class="fa-brands fa-github"></i></button></a>
+                    </div><!--btn-social-->
+                </div><!--txt-sobre-->
+            </div><!--flex-->
+        </div><!--interface-->
+    </section><!--sobre-->
+
+    <section class="meio-do-site">
+        <div class="interface">
+            <div class="flex">
+                <div class="txt-meio-site">
+                    <h1>PUBLICO ALVO<span>.</span></h1>
+                    <p>O público-alvo das casas de acolhimento para mulheres geralmente são mulheres em situação de vulnerabilidade e risco, como vítimas de violência doméstica, abuso sexual, tráfico humano, em situação de rua, em contexto de migração forçada, entre outras situações de fragilidade. Essas casas oferecem um ambiente seguro e apoio psicossocial, jurídico e muitas vezes também assistência médica para ajudar essas mulheres a reconstruírem suas vidas e recuperarem sua autonomia. Acompanhadas ou não de seus filhos com idade abaixo de 18 anos.</p>
+                </div><!--txt-meio-site-->
+
+                <div class="img-meio-site">
+                    <img src="../img/womens.png" alt="">
+                </div><!--img-meio-site-->
+            </div><!--flex-->
+        </div><!--interface-->
+    </section><!--meio-do-site-->
+    
+    <script src="script.js"></script>
+</body>
+</html>
+
+<?php include("blades/footer.php");?>
